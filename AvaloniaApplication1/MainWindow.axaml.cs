ebaeb4af0aeabfff0caefd2f60e4143574b2fd00
sheenshu <akademiashag009@gmail.com>
2026-03-14 13:49:25 +0300

    Добавьте файлы проекта.

---
+++ AvaloniaApplication1/MainWindow.axaml.cs
@@ -0,0 +1,61 @@
+using Avalonia.Controls;
+using Avalonia.Input;
+using System;
+using System.Runtime.InteropServices;
+
+namespace AvaloniaApplication1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private string GetX()
+        {
+            //ax2 + bx + c = 0
+            double a = Convert.ToDouble(TextFiled.Text);
+            double b = Convert.ToDouble(TextFiled2.Text);
+            double c = Convert.ToDouble(TextFiled3.Text);
+
+            double D = b * b - 4 * a * c;
+            double x1 = (-b + Math.Sqrt(D)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(D)) / (2 * a);
+
+            if (D < 0)
+            {
+           
+                return "Корней нет";
+            }
+
+            if (D == 0)
+            {
+                if (x1 < x2)
+                {
+                    return $"D = 0 => \nx = {x2}";
+                }
+                else return $"D = 0 => \nx = {x1}";
+            }
+
+            string foundX = $"x1 = {x1} \nx2 = {x2}";
+
+            return foundX;
+        }
+
+        private void TextBox_KeyDown(object? sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                GetX();
+            }
+        }
+
+        private void FindX(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            PrintX.Text = GetX();
+        }
+    }
+}
